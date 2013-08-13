cc09b5ed22d736b482a08a80bd66d3b6100f593a
yaonimingsanqian <529670074@qq.com>
2013-08-13 23:06:01 +0800

    真机测试通过

--- interface/SpeechRecognition/GoogleRecognize/ASGoogleVoiceRecognizer.mm
+++ interface/SpeechRecognition/GoogleRecognize/ASGoogleVoiceRecognizer.mm
@@ -53,7 +53,7 @@
         
         uploadQueue = [[NSMutableArray alloc]init];
         
-        soundStrengthThreshold = 250;
+        soundStrengthThreshold = 150;
         soundStrengthArray = [[NSMutableArray alloc]init];
         
         uploadDataArray = [[NSMutableArray alloc]init];
@@ -145,7 +145,7 @@
     
     [soundStrengthArray addObject:[NSNumber numberWithInt:soundStrongh]];
     
-    if (soundStrongh > /*soundStrengthThreshold*/40)
+    if (soundStrongh > soundStrengthThreshold)
     {
         //soundStrengthThreshold -= 1;
         canRecgnise = YES;
